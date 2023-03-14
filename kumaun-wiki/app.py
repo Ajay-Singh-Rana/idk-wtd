a85b0dac347d21eb4df75ef8bfe238486a412a2d
Ajay Singh Rana <ajaysinghrana494@gmail.com>
2023-03-14 18:20:45 +0530

    Learn To Access Form Data And Redirect Using Flask

--- kumaun-wiki/app.py
+++ kumaun-wiki/app.py
@@ -1,6 +1,6 @@
 # h3avren
 
-from flask import Flask, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -11,5 +11,26 @@ def home():
 
 @app.route('/search', methods = ['POST'])
 def search():
-    return render_template('search_results_page.html', data = request.form)
+    print('hererwrwrwwrwwrwrwqrwrqwrqwer')
+    print(request.method)
+    print(request.form)
+    if request.method == 'POST':
+        topic = request.form['search_value'].lower()
+        with open('articles.txt','r') as file:
+            text = file.read()
+        if(topic in text):
+            return render_template(f"{topic.replace(' ', '_')}.html")
+        else:
+            return "Error..!"
 
+@app.route('/login_page')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        print(request.form)
+        print(request.form['username'])
+        print(request.form['password'])
+    return redirect(url_for('home'))
